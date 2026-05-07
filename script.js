5dd6744bdaad591219ea64c0b7df9c08d5c0d3fb
talha485 <talhachaudhary120@gmail.com>
2026-05-08 00:20:41 +0500

    improve slider functionality

--- script.js
+++ script.js
@@ -1,52 +1,81 @@
-const slides = document.querySelector('.slides');
-if (slides) {
-
+document.addEventListener("DOMContentLoaded", function () {
     let index = 0;
+    let slides = document.querySelector(".slides");
+    let totalSlides = document.querySelectorAll(".slide").length;
+    let nextSlide = document.querySelector(".arrow-next");
+    let prevSlide = document.querySelector(".arrow-prev");
+    let sliderInterval;
 
 
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
-
-    setInterval(function () {
-        index++;
-
-        if (index >= totalSlides) {
+    function moveSlider() {
+        if (!slides) return;
+        if (index >= totalSlides)
             index = 0;
+        if (index < 0)
+            index = totalSlides - 1;
+
+        slides.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function initSlider() {
+        sliderInterval = setInterval(function () {
+            index++;
+            moveSlider();
+        }, 2000);
+    }
+    function stopSlider() {
+        clearInterval(sliderInterval);
+    }
+    slides.addEventListener("mouseenter", stopSlider);
+    slides.addEventListener("mouseleave", initSlider);
+
+    function initContactForm() {
+        let form = document.getElementById("contactForm");
+
+        if (form) {
+            form.addEventListener("submit", function (e) {
+                e.preventDefault();
+                alert("Form submitted");
+                form.reset();
+            });
+        }
+    }
+
+    function loadHeaderFooter() {
+        fetch("header.html")
+            .then(res => res.text())
+            .then(data => {
+                document.getElementById("header").innerHTML = data;
+                initSliderButtons();
+            });
+
+        fetch("footer.html")
+            .then(res => res.text())
+            .then(data => {
+                document.getElementById("footer").innerHTML = data;
+            });
+    }
+
+    function initSliderButtons() {
+
+        if (nextSlide && prevSlide) {
+
+            nextSlide.addEventListener("click", function () {
+                index++;
+                moveSlider();
+            });
+
+            prevSlide.addEventListener("click", function () {
+                index--;
+                moveSlider();
+            });
+
+
         }
 
-        slides.style.transform = "translateX(-" + index * 100 + "%)";
-    }, 2000);
-}
+    }
+    initSlider();
+    initContactForm();
+    loadHeaderFooter();
 
-let form = document.getElementById('contactForm');
-if (form) {
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        let name = document.getElementById('name').value;
-        let email = document.getElementById('email').value;
-        let phone = document.getElementById('phone').value;
-        let message = document.getElementById('message').value;
-
-        alert(`${name} with Phone number ${phone} & email ${email} has sent the following message: ${message}`);
-
-        form.reset();
-    });
-}
-//Header
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("header").innerHTML = data;
-        });
-});
-//Footer
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("footer.html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("footer").innerHTML = data;
-        });
 });
