ce64c450e4c3de5b051dee42efe3dc2bcf52c1f6
talha485 <talhachaudhary120@gmail.com>
2026-06-01 00:05:28 +0500

    integerate tailwind css on about-us page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./**/*.html", "./src/**/*.js", "./products.js"],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
